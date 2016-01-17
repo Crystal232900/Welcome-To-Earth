ddd96b8498c2ee5e32d7e1deb47b7d5a530acdac
Crystal232900 <crystalsphone924@gmail.com>
2016-01-16 18:23:20 -0700

    updated files

---
+++ WelcomeToEarth/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeToEarth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {Console.WriteLine("Welcome To Earth");
+            Console.ReadLine();
+            Console.WriteLine("Be Happy!");
+            Console.ReadLine();
+        }
+    }
+}
